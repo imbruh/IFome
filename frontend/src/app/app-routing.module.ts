783c11d9a68bd0a96b3a76f767e6adf390b02648
kevenallan <kevenallan98@gmail.com>
2022-09-07 21:07:37 -0300

    Ajuste no label do cnpj e na rota de cadastro empresa

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: LoginComponent
     },
     {
-        path:"cadastrar-empresa",
+        path:"cadastro-empresa",
         component:CadastrarEmpresaComponent
     }
 ];
